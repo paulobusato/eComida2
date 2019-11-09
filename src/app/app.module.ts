e96504261052de686851e8f5da760194d4d9998f
paulobusato <paulo.busato2@gmail.com>
2019-11-09 19:39:27 -0300

    adicionado estrutura básica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AdministrativoModule } from './administrativo/administrativo.module';
+import { ClienteModule } from './cliente/cliente.module';
+import { EstabelecimentoModule } from './estabelecimento/estabelecimento.module';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    AdministrativoModule,
+    ClienteModule,
+    EstabelecimentoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/estabelecimento/estabelecimento.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EstabelecimentoRoutingModule } from './estabelecimento-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EstabelecimentoRoutingModule
+  ]
+})
+export class EstabelecimentoModule { }
