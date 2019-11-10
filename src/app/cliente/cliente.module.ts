dd9f445af3818abf39387b3104b408a2d24a415d
paulobusato <paulo.busato2@gmail.com>
2019-11-10 08:46:32 -0300

    adicionado cliente login

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClienteRoutingModule } from './cliente-routing.module';
+import { ClienteComponent } from './cliente.component';
+import { ClienteLoginComponent } from './cliente-login/cliente-login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ClienteComponent, ClienteLoginComponent],
   imports: [
     CommonModule,
     ClienteRoutingModule
