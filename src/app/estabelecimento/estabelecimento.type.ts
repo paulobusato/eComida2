d669c6402d4e4c0edcf3fc1a7cedc138b9465c5b
paulobusato <paulo.busato2@gmail.com>
2019-11-11 13:39:39 -0300

    add estabelecimento interface

--- src/app/estabelecimento/estabelecimento.type.ts
+++ src/app/estabelecimento/estabelecimento.type.ts
@@ -1,3 +1,16 @@
 export interface Estabelecimento {
-  r
+  idEstabelecimento: number;
+  razaoSocial: string;
+  nomeFantasia: string;
+  cnpj: number;
+  status: string;
+  email: string;
+  senha?: string;
+  telefone: number;
+  cep: number;
+  logradouro: string;
+  numero: number;
+  bairro: string;
+  cidade: string;
+  uf: string;
 }
