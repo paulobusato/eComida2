fda5da1d54ac46a1d7cb236c25cbd90d4aff67c0
paulobusato <paulo.busato2@gmail.com>
2019-11-12 01:20:34 -0300

    finalizado layout sacola

--- src/app/cliente/cliente.type.ts
+++ src/app/cliente/cliente.type.ts
@@ -30,3 +30,8 @@ export interface Componente {
   quantidade: number;
   items: string[];
 }
+
+export interface Pedido {
+  estabelecimento: Estabelecimento;
+  produtos: Produto[];
+}
--- src/app/cliente/sacola/sacola.component.ts
+++ src/app/cliente/sacola/sacola.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pedido } from '../cliente.type';
 
 @Component({
   selector: 'app-sacola',
@@ -6,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sacola.component.scss']
 })
 export class SacolaComponent implements OnInit {
+  pedido: Pedido = {
+    estabelecimento: {
+      bairro: 'Cachoeiro',
+      distancia: 5.5,
+      imgUrl: '',
+      nome: 'Bobs',
+      rating: 4.3,
+      tempoEspera: 20,
+    },
+    produtos: [
+      {
+        imgUrl: 'https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/af7f7d95-85ad-4e08-a2bb-edbb3555fab1/201806062016_40603626.jpg',
+        titulo: 'Marmitex churrasco 500g, coca cola 1',
+        descricao: 'Caixa p + 1 acompanhamento + 1 molho',
+        valor: 39.10,
+        componentes: [
+          {
+            descricao: 'Escolha sua carne',
+            quantidade: 2,
+            items: [
+              'Lombo de porco',
+              'Linguiça de churrasco',
+              'Carne de Boi',
+            ]
+          },
+          {
+            descricao: 'Escolha sua carne',
+            quantidade: 2,
+            items: [
+              'Lombo de porco',
+              'Linguiça de churrasco',
+              'Carne de Boi',
+            ]
+          },
+        ],
+      }
+    ]
+  };
 
   constructor() { }
 
