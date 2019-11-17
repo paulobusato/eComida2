e5360b9cd96edb6c92355c5a4a4864184ae779f7
paulobusato <paulo.busato2@gmail.com>
2019-11-17 19:33:06 -0300

    add rodape cliente componente e ajustado margin desktop mode

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LimiteCaracteresPipe } from './limite-caracteres.pipe';
 import { FooterComponent } from './footer/footer.component';
 import { MatToolbarModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [LimiteCaracteresPipe, FooterComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
+    FlexLayoutModule,
   ],
   exports: [
     LimiteCaracteresPipe,
