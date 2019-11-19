978bad808952932f8be8e44dfc86dbdda8df8de5
paulobusato <paulo.busato2@gmail.com>
2019-11-19 19:23:12 -0300

    add consulta cliente

--- dist/modelo/dao/ClienteDao.php
+++ dist/modelo/dao/ClienteDao.php
@@ -1,11 +1,64 @@
 <?php
 
+require_once 'Dao.php';
+require_once '../modelo/entidade/Cliente.php';
+
 class ClienteDao {
 
-  public static function consultar() {
+  public static function consultar($idCliente = '') {
+    $clientes = array();
+
+    if ($idCliente != '') {
+      $sql = "
+        SELECT *
+        FROM CLIENTE C
+        WHERE C.IDCLIENTE = {$idCliente}
+      ";
+    } else {
+      $sql = "
+        SELECT *
+        FROM CLIENTE C
+      ";
+    }
+
+    $db_clientes = Dao::consultar($sql);
+
+    foreach ($db_clientes as $db_cliente) {
+      if ($idCliente != '') {
+        return new Cliente(
+          $db_cliente->IDCLIENTE,
+          $db_cliente->NOME,
+          $db_cliente->CPF,
+          $db_cliente->EMAIL,
+          '',
+          $db_cliente->TELEFONE,
+          $db_cliente->CEP,
+          $db_cliente->LOGRADOURO,
+          $db_cliente->NUMERO,
+          $db_cliente->BAIRRO,
+          $db_cliente->CIDADE,
+          $db_cliente->UF
+        );
+      } else {
+        array_push($clientes, new Cliente(
+          $db_cliente->IDCLIENTE,
+          $db_cliente->NOME,
+          $db_cliente->CPF,
+          $db_cliente->EMAIL,
+          '',
+          $db_cliente->TELEFONE,
+          $db_cliente->CEP,
+          $db_cliente->LOGRADOURO,
+          $db_cliente->NUMERO,
+          $db_cliente->BAIRRO,
+          $db_cliente->CIDADE,
+          $db_cliente->UF
+        ));
+      }
+    }
+
+    return $clientes;
   }
-
-
 
   public static function login($email, $senha) {
     $sql = "
