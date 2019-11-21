8ba33603b7610b49a1473f4dcb17b116f80c3615
paulobusato <paulo.busato2@gmail.com>
2019-11-21 19:20:43 -0300

    fix dao ordem de drop e criacao de tabela

--- dist/modelo/dao/Dao.php
+++ dist/modelo/dao/Dao.php
@@ -9,14 +9,14 @@ class Dao extends Conexao    {
     parent::executar("
       DROP TABLE categoria;
       DROP TABLE cliente;
+      DROP TABLE estabelecimento;
+      DROP TABLE produto;
       DROP TABLE componente;
       DROP TABLE componenteitem;
-      DROP TABLE estabelecimento;
       DROP TABLE pedido;
       DROP TABLE pedidoitem;
-      DROP TABLE produto;
-      DROP TABLE promocao;
       DROP TABLE pedidoitemcomponente;
+      DROP TABLE promocao;
     ");
 
     parent::executar("
@@ -29,16 +29,20 @@ class Dao extends Conexao    {
     ");
 
     parent::executar("
-      CREATE TABLE PROMOCAO (
-        IDPROMOCAO INT NOT NULL AUTO_INCREMENT,
-        IDCATEGORIA INT NOT NULL,
-        DESCRICAO VARCHAR(255) NOT NULL,
-        DATAINICIO DATETIME NOT NULL,
-        DATAFIM DATETIME NOT NULL,
-        PERCENTUAL FLOAT NOT NULL,
-        PRIMARY KEY(IDPROMOCAO),
-        FOREIGN KEY (IDCATEGORIA)
-          REFERENCES CATEGORIA(IDCATEGORIA)
+      CREATE TABLE CLIENTE (
+        IDCLIENTE INT NOT NULL AUTO_INCREMENT,
+        NOME VARCHAR(255) NOT NULL,
+        CPF VARCHAR(255) NOT NULL,
+        EMAIL VARCHAR(255) NOT NULL,
+        SENHA VARCHAR(255) NOT NULL,
+        TELEFONE VARCHAR(255) NOT NULL,
+        CEP VARCHAR(255) NOT NULL,
+        LOGRADOURO VARCHAR(255) NOT NULL,
+        NUMERO VARCHAR(255) NOT NULL,
+        BAIRRO VARCHAR(255) NOT NULL,
+        CIDADE VARCHAR(255) NOT NULL,
+        UF VARCHAR(255) NOT NULL,
+        PRIMARY KEY(IDCLIENTE)
       );
     ");
 
@@ -61,38 +65,6 @@ class Dao extends Conexao    {
         CIDADE VARCHAR(255) NOT NULL,
         UF VARCHAR(255) NOT NULL,
         PRIMARY KEY(IDESTABELECIMENTO)
-      );
-    ");
-    parent::executar("
-      CREATE TABLE CLIENTE (
-        IDCLIENTE INT NOT NULL AUTO_INCREMENT,
-        NOME VARCHAR(255) NOT NULL,
-        CPF VARCHAR(255) NOT NULL,
-        EMAIL VARCHAR(255) NOT NULL,
-        SENHA VARCHAR(255) NOT NULL,
-        TELEFONE VARCHAR(255) NOT NULL,
-        CEP VARCHAR(255) NOT NULL,
-        LOGRADOURO VARCHAR(255) NOT NULL,
-        NUMERO VARCHAR(255) NOT NULL,
-        BAIRRO VARCHAR(255) NOT NULL,
-        CIDADE VARCHAR(255) NOT NULL,
-        UF VARCHAR(255) NOT NULL,
-        PRIMARY KEY(IDCLIENTE)
-      );
-    ");
-
-    parent::executar("
-      CREATE TABLE PEDIDO (
-        IDPEDIDO INT NOT NULL AUTO_INCREMENT,
-        IDESTABELECIMENTO INT NOT NULL,
-        IDCLIENTE INT NOT NULL,
-        DATA DATETIME NOT NULL,
-        VALOR FLOAT NOT NULL,
-        PRIMARY KEY(IDPEDIDO),
-        FOREIGN KEY (IDCLIENTE)
-          REFERENCES CLIENTE(IDCLIENTE),
-        FOREIGN KEY (IDESTABELECIMENTO)
-          REFERENCES ESTABELECIMENTO(IDESTABELECIMENTO)
       );
     ");
 
@@ -139,6 +111,21 @@ class Dao extends Conexao    {
     ");
 
     parent::executar("
+      CREATE TABLE PEDIDO (
+        IDPEDIDO INT NOT NULL AUTO_INCREMENT,
+        IDESTABELECIMENTO INT NOT NULL,
+        IDCLIENTE INT NOT NULL,
+        DATA DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        VALOR FLOAT NOT NULL,
+        PRIMARY KEY(IDPEDIDO),
+        FOREIGN KEY (IDCLIENTE)
+          REFERENCES CLIENTE(IDCLIENTE),
+        FOREIGN KEY (IDESTABELECIMENTO)
+          REFERENCES ESTABELECIMENTO(IDESTABELECIMENTO)
+      );
+    ");
+
+    parent::executar("
       CREATE TABLE PEDIDOITEM (
         IDPEDIDO INT NOT NULL,
         IDPEDIDOITEM INT NOT NULL AUTO_INCREMENT,
@@ -171,15 +158,26 @@ class Dao extends Conexao    {
       );
     ");
 
-    parent::executar("ALTER TABLE `pedido` CHANGE `DATA` `DATA` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;");
-
-    parent::executar("INSERT INTO `cliente` (`IDCLIENTE`, `NOME`, `CPF`, `EMAIL`, `SENHA`, `TELEFONE`, `CEP`, `LOGRADOURO`, `NUMERO`, `BAIRRO`, `CIDADE`, `UF`) VALUES (NULL, 'Paulo Henrique Busato', '32132412389', 'paulo@paulo.com', '123', '23432139988', '23854322', 'Logradouro', '10', 'Bairro', 'Cidade', 'UF')");
-
-    parent::executar("INSERT INTO `estabelecimento` (`IDESTABELECIMENTO`, `RAZAOSOCIAL`, `NOMEFANTASIA`, `CNPJ`, `STATUS`, `RATING`, `IMGURL`, `EMAIL`, `SENHA`, `TELEFONE`, `CEP`, `LOGRADOURO`, `NUMERO`, `BAIRRO`, `CIDADE`, `UF`) VALUES (NULL, 'eComida', 'eComida', '99999999999999', 'P', '4', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThlFOqWox5fl0i82VmIvxAAuFUoQ9NqcIoH4EfwFnMbG6B8BynnA&s', 'paulo@paulo.com', '123', '28999999999', '84520369', 'Av Francisco Mardegan', '02', 'Boa Vista', 'Cachoeiro de Itapemirim', 'ES')");
+    parent::executar("
+      CREATE TABLE PROMOCAO (
+        IDPROMOCAO INT NOT NULL AUTO_INCREMENT,
+        IDCATEGORIA INT NOT NULL,
+        DESCRICAO VARCHAR(255) NOT NULL,
+        DATAINICIO DATETIME NOT NULL,
+        DATAFIM DATETIME NOT NULL,
+        PERCENTUAL FLOAT NOT NULL,
+        PRIMARY KEY(IDPROMOCAO),
+        FOREIGN KEY (IDCATEGORIA)
+          REFERENCES CATEGORIA(IDCATEGORIA)
+      );
+    ");
 
     parent::executar("INSERT INTO `categoria` (`IDCATEGORIA`, `DESCRICAO`, `IMAGEMURL`) VALUES (NULL, 'Acai', 'https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2018/04/21/salada.jpg')");
     parent::executar("INSERT INTO `categoria` (`IDCATEGORIA`, `DESCRICAO`, `IMAGEMURL`) VALUES (NULL, 'Lanches', 'https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2018/04/21/salada.jpg')");
     parent::executar("INSERT INTO `categoria` (`IDCATEGORIA`, `DESCRICAO`, `IMAGEMURL`) VALUES (NULL, 'Salada', 'https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2018/04/21/salada.jpg')");
+
+    parent::executar("INSERT INTO `cliente` (`IDCLIENTE`, `NOME`, `CPF`, `EMAIL`, `SENHA`, `TELEFONE`, `CEP`, `LOGRADOURO`, `NUMERO`, `BAIRRO`, `CIDADE`, `UF`) VALUES (NULL, 'Paulo Henrique Busato', '32132412389', 'paulo@paulo.com', '123', '23432139988', '23854322', 'Logradouro', '10', 'Bairro', 'Cidade', 'UF')");
+    parent::executar("INSERT INTO `estabelecimento` (`IDESTABELECIMENTO`, `RAZAOSOCIAL`, `NOMEFANTASIA`, `CNPJ`, `STATUS`, `RATING`, `IMGURL`, `EMAIL`, `SENHA`, `TELEFONE`, `CEP`, `LOGRADOURO`, `NUMERO`, `BAIRRO`, `CIDADE`, `UF`) VALUES (NULL, 'eComida', 'eComida', '99999999999999', 'P', '4', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThlFOqWox5fl0i82VmIvxAAuFUoQ9NqcIoH4EfwFnMbG6B8BynnA&s', 'paulo@paulo.com', '123', '28999999999', '84520369', 'Av Francisco Mardegan', '02', 'Boa Vista', 'Cachoeiro de Itapemirim', 'ES')");
 
     parent::executar("INSERT INTO `produto` (`IDPRODUTO`, `IDESTABELECIMENTO`, `TITULO`, `DESCRICAO`, `VALOR`, `IMGURL`) VALUES (NULL, 1, 'Combo p - indicamos para 1 a 2 pessoas', 'Caixa p + 1 acompanhamento + 1 molho', '39.10', 'https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/af7f7d95-85ad-4e08-a2bb-edbb3555fab1/201806062016_40603626.jpg')");
     parent::executar("INSERT INTO `componente` (`IDPRODUTO`, `IDCOMPONENTE`, `DESCRICAO`, `QUANTIDADE`, `OBRIGATORIO`) VALUES ('1', NULL, 'Escolha seu frango frito crocante', '1', 'S')");
@@ -232,4 +230,4 @@ class Dao extends Conexao    {
   }
 }
 
-// Dao::resetarDB();
+Dao::resetarDB();
