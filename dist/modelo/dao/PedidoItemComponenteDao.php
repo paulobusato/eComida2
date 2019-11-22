d5254e0016925cf0a4647a4d1d87baeddaabcb1b
paulobusato <paulo.busato2@gmail.com>
2019-11-22 12:04:32 -0300

    corrigido consulta para inclusão do componente

--- dist/modelo/dao/PedidoItemComponenteDao.php
+++ dist/modelo/dao/PedidoItemComponenteDao.php
@@ -6,8 +6,8 @@ class PedidoItemComponenteDao {
 
   public static function inserir($idPedido, $idPedidoItem, $idComponente, $idComponenteItem) {
     $sql = "
-      INSERT INTO `pedidoitemcomponente` (`IDPEDIDO`, `IDPEDIDOITEM`, `IDCOMPONENTE`, `IDCOMPONENTEITEM`)
-      VALUES ('{$idPedido}', '{$idPedidoItem}', '{$idComponente}' , '{$idComponenteItem}');
+      INSERT INTO `pedidoitemcomponente` (`IDPEDIDO`, `IDCOMPONENTE`, `IDCOMPONENTEITEM`)
+      VALUES ('{$idPedido}', '{$idComponente}' , '{$idComponenteItem}');
     ";
 
     Dao::executar($sql);
