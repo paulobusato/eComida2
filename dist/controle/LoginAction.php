f6c5036c47d0c9e0fa120d1ebed1fc811d9cb414
paulobusato <paulo.busato2@gmail.com>
2019-11-23 13:45:18 -0300

    LoginAction gerando e retornando Token de Autenticacao

--- dist/controle/LoginAction.php
+++ dist/controle/LoginAction.php
@@ -3,19 +3,55 @@
 header('Access-Control-Allow-Origin: http://localhost:4200');
 header('Content-type: application/json');
 
-$method = $_SERVER["REQUEST_METHOD"];
+require_once '../modelo/dao/EstabelecimentoDao.php';
+require_once('../vendor/autoload.php');
+use \Firebase\JWT\JWT;
 
-var_dump($_SERVER);
+define('SECRET_KEY', 'Super-Secret-Key');
+define('ALGORITHM', 'HS256');
 
-switch ($method) {
-  case 'GET':
-    if ($_GET["cliente"]) {
-      echo json_encode("true");
-    } else if ($_GET["estabelecimento"]) {
-      echo json_encode("false");
-    }
-    break;
-  default:
-    echo 'Não existe';
-    break;
+$requisicao = json_decode(file_get_contents("php://input"));
+
+$tipoUsuario = $requisicao->tipoUsuario;
+$email = $requisicao->email;
+$senha = $requisicao->senha;
+
+if ($tipoUsuario == 'E') {
+  $idEstabelecimento = EstabelecimentoDao::login($email, $senha);
+
+  if ($idEstabelecimento) {
+    $iat = time();
+    $nbf = $iat + 10;
+    $exp = $iat + 60;
+
+    $token = array(
+      "iss" => "http://example.org",
+      "aud" => "http://example.com",
+      "iat" => $iat,
+      "nbf" => $nbf,
+      "exp" => $exp,
+      "data" => array(
+        "idEstabelecimento" => $idEstabelecimento
+        )
+      );
+
+    http_response_code(200);
+
+    $jwt = JWT::encode($token, SECRET_KEY);
+
+    $dados_inserido = array(
+      "token" => $jwt,
+      "idEstabelecimento" => $idEstabelecimento,
+      "status" => "sucesso",
+      "mensagem" => "Logado com sucesso"
+    );
+  } else {
+    $dados_inserido = array(
+      "status" => "invalido",
+      "mensagem" => "Requisição Inválida"
+    ); 
+  }
+
 }
+
+echo json_encode($dados_inserido);
