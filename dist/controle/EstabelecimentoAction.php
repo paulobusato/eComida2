da8a2ba83540e0115e2f281f0fdc3573d44c87b9
paulobusato <paulo.busato2@gmail.com>
2019-11-24 08:40:28 -0300

    removido switch sem autenticacao e refatorado cors

--- dist/controle/EstabelecimentoAction.php
+++ dist/controle/EstabelecimentoAction.php
@@ -1,23 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-type: application/json');
-header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Credentials: true');
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-      header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-      header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-  exit(0);
-}
-
-require_once '../modelo/dao/Dao.php';
-require_once '../modelo/entidade/Estabelecimento.php';
+require_once 'cors/cors.php';
 require_once '../modelo/dao/EstabelecimentoDao.php';
 require_once '../vendor/autoload.php';
 use \Firebase\JWT\JWT;
@@ -33,8 +15,21 @@ try {
   $idEstabelecimento = $decoded->data->idEstabelecimento;
 
   http_response_code(200);
-  
-  $response = EstabelecimentoDao::consultar($idEstabelecimento)[0];
+
+  $json_str = file_get_contents('php://input');
+  $json_obj = json_decode($json_str);
+  $method = $_SERVER["REQUEST_METHOD"];
+
+  switch ($method) {
+    case 'GET':
+      $response = EstabelecimentoDao::consultar($idEstabelecimento)[0];
+      break;
+    case 'PUT':
+      EstabelecimentoDao::alterar($json_obj);
+    default:
+      echo 'Não existe';
+    break;
+  }  
 
 } catch (Exception $e) {
   
@@ -48,50 +43,3 @@ try {
 }
 
 echo json_encode($response);
-exit;
-
-$json_str = file_get_contents('php://input');
-$json_obj = json_decode($json_str);
-
-switch ($method) {
-  case 'GET':
-    if (isset($_GET["idEstabelecimento"])) {
-      echo json_encode(EstabelecimentoDao::consultar($_GET["idEstabelecimento"]));
-    } else {
-      echo json_encode(EstabelecimentoDao::consultar());
-    }
-    break;
-  case 'POST':
-    $estabelecimento = new Estabelecimento(
-      '',
-      $json_obj->razaoSocial,
-      $json_obj->nomeFantasia,
-      $json_obj->cnpj,
-      $json_obj->status,
-      '0',
-      '',
-      $json_obj->email,
-      $json_obj->senha,
-      $json_obj->telefone,
-      $json_obj->cep,
-      $json_obj->logradouro,
-      $json_obj->numero,
-      $json_obj->bairro,
-      $json_obj->cidade,
-      $json_obj->uf
-    );
-    EstabelecimentoDao::inserir($estabelecimento);
-    echo json_encode("true");
-    break;
-  case 'PUT':
-    EstabelecimentoDao::alterar($estabelecimento);
-    echo json_encode("true");
-    break;
-  case 'DELETE':
-    EstabelecimentoDao::excluir($estabelecimento->idEstabelecimento);
-    echo json_encode("true");
-    break;
-  default:
-    echo 'Não existe';
-    break;
-}
