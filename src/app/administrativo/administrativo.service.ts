e54110e4f45bb21c5c2433ebde29d30110485d2b
paulobusato <paulo.busato2@gmail.com>
2019-11-24 08:47:10 -0300

    alterado ordem dos metodos e refatorado link para variaveis

--- src/app/administrativo/administrativo.service.ts
+++ src/app/administrativo/administrativo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Estabelecimento } from '../estabelecimento/estabelecimento.type';
 import { map } from 'rxjs/operators';
@@ -9,20 +9,29 @@ import { Pedido } from '../cliente/cliente.type';
   providedIn: 'root'
 })
 export class AdministrativoService {
+  urlEstabelecimento = `http://localhost/eComida2/dist/controle/estabelecimentoaction.php`;
+  urlPedido = `http://localhost/eComida2/dist/controle/pedidoaction.php`;
 
   constructor(
     private http: HttpClient,
   ) { }
 
+  httpOpcoes = {
+    headers: new HttpHeaders({
+      'Content-type': 'application/json',
+    }),
+  };
+
   obterEstabelecimento(): Observable<Estabelecimento> {
-    return this.http.get<Estabelecimento>(
-      `http://localhost/eComida2/dist/controle/estabelecimentoaction.php`
-    );
+    return this.http.get<Estabelecimento>(this.urlEstabelecimento);
+  }
+
+  atualizarCadastroEstabelecimento(estabelecimento: Estabelecimento): Observable<void> {
+    return this.http.put<void>(this.urlEstabelecimento, estabelecimento, this.httpOpcoes);
   }
 
   obterPedidos(): Observable<Pedido[]> {
-    return this.http.get<Pedido[]>(
-      `http://localhost/eComida2/dist/controle/pedidoaction.php`
-    );
+    return this.http.get<Pedido[]>(this.urlPedido);
   }
+
 }
