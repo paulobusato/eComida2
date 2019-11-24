b12956b4f0d29fb41ad8932507a1275349bd8949
paulobusato <paulo.busato2@gmail.com>
2019-11-24 11:03:23 -0300

    ajustado metodo obter estabelecimentos

--- src/app/cliente/cliente.service.ts
+++ src/app/cliente/cliente.service.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ClienteService {
+  urlEstabelecimento = 'http://localhost/eComida2/dist/controle/estabelecimentoaction.php';
   produtoAtivado: Produto;
   estabelecimentoAtivo: Estabelecimento;
   clienteAtivo: Cliente;
@@ -20,14 +21,8 @@ export class ClienteService {
     return this.http.get<Categoria[]>('http://localhost/eComida2/dist/controle/categoriaaction.php');
   }
 
-  obterEstabelecimentos(idEstabelecimento?: number): Observable<Estabelecimento[]> {
-    if (idEstabelecimento) {
-      return this.http.get<Estabelecimento[]>(
-        `http://localhost/eComida2/dist/controle/estabelecimentoaction.php?idEstabelecimento=${idEstabelecimento}`
-      );
-    } else {
-      return this.http.get<Estabelecimento[]>('http://localhost/eComida2/dist/controle/estabelecimentoaction.php');
-    }
+  obterEstabelecimentos(): Observable<Estabelecimento[]> {
+    return this.http.get<Estabelecimento[]>(this.urlEstabelecimento);
   }
 
   obterProdutos(idEstabelecimento?: number): Observable<Produto[]> {
