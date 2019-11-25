1655ff40c845862d6fe753814b5d1c49f4277fbe
paulobusato <paulo.busato2@gmail.com>
2019-11-25 12:05:44 -0300

    add autenticacao e consulta por 1 produto ou uma lista

--- dist/controle/ProdutoAction.php
+++ dist/controle/ProdutoAction.php
@@ -1,35 +1,54 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-type: application/json');
-header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Methods: *');
-
-require_once '../modelo/dao/Dao.php';
-require_once '../modelo/entidade/Produto.php';
-require_once '../modelo/entidade/Componente.php';
-require_once '../modelo/entidade/ComponenteItem.php';
-require_once '../modelo/entidade/Estabelecimento.php';
+require_once 'cors/cors.php';
 require_once '../modelo/dao/ProdutoDao.php';
-require_once '../modelo/dao/ComponenteDao.php';
-require_once '../modelo/dao/ComponenteItemDao.php';
-require_once '../modelo/dao/EstabelecimentoDao.php';
+require_once '../vendor/autoload.php';
+use \Firebase\JWT\JWT;
 
-$method = $_SERVER["REQUEST_METHOD"];
+$jwt = substr(apache_request_headers()["Authorization"], 7);
 
-function onMethodGet() {
-  if (isset($_GET["idEstabelecimento"])) {
-    return ProdutoDao::consultar((int) $_GET["idEstabelecimento"]);
-  } else {
-    return new Produto();
+define('SECRET_KEY', 'Super-Secret-Key');
+define('ALGORITHM', 'HS256');
+
+try {
+  JWT::$leeway = 10;
+  $decoded = JWT::decode($jwt, SECRET_KEY, array(ALGORITHM));
+  if (isset($decoded->data->idEstabelecimento)) {
+    $idEstabelecimento = $decoded->data->idEstabelecimento;
   }
+  if (isset($decoded->data->idCliente)) {
+    $idCliente = $decoded->data->idCliente;
+  }
+
+  http_response_code(200);
+
+  $json_str = file_get_contents('php://input');
+  $json_obj = json_decode($json_str);
+  $method = $_SERVER["REQUEST_METHOD"];
+  
+  if (isset($idEstabelecimento)) {
+    switch ($method) {
+      case 'GET':
+        if (isset($_GET["idProduto"])) {
+          $response = ProdutoDao::consultar($idEstabelecimento, $_GET["idProduto"]);
+        } else {
+          $response = ProdutoDao::consultar($idEstabelecimento);
+        }
+        break;
+      default:
+        $response = 'Não existe';
+      break;
+    }
+  }
+
+} catch (Exception $e) {
+  
+  http_response_code(401);
+
+  $response = array(
+    "jwt" => $jwt,
+    "status" => "error",
+    "mensagem" => $e->getMessage()
+  );
 }
 
-switch ($method) {
-  case 'GET':
-    $produtos = onMethodGet();
-    echo json_encode($produtos);
-    break;
-  default:
-    echo 'Não existe';
-    break;
-}
+echo json_encode($response);
