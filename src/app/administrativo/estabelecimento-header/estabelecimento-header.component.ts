ec5b8ee562171de9f5e705ea19cd00328296a37d
paulobusato <paulo.busato2@gmail.com>
2019-12-01 22:06:19 -0300

    add opcao de sair administrativo

--- src/app/administrativo/estabelecimento-header/estabelecimento-header.component.ts
+++ src/app/administrativo/estabelecimento-header/estabelecimento-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-estabelecimento-header',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstabelecimentoHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSair(): void{
+    localStorage.clear();
+    this.router.navigate(['/landing']);
   }
 
 }
