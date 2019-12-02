30d19bcbf1cb1cb0593afa2f5335e88cbf0f7910
paulobusato <paulo.busato2@gmail.com>
2019-12-02 01:10:29 -0300

    add propriedade componentes

--- dist/modelo/entidade/PedidoItem.php
+++ dist/modelo/entidade/PedidoItem.php
@@ -6,14 +6,16 @@ class PedidoItem
     public $produto;
     public $quantidade;
     public $valor;
+    public $componentes;
 
-    public function __construct($idPedido = '', $idPedidoItem = '', $produto = '', $quantidade = '', $valor = '')
+    public function __construct($idPedido = '', $idPedidoItem = '', $produto = '', $quantidade = '', $valor = '', $componentes = '')
     {
         $this->idPedido = $idPedido;
         $this->idPedidoItem = $idPedidoItem;
         $this->produto = $produto;
         $this->quantidade = $quantidade;
         $this->valor = $valor;
+        $this->componentes = $componentes;
     }
 }
 
